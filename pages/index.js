be65363d5a7d9e6b010fb892744de40e52aa4157
MatiCastr0 <maticastrook@gmail.com>
2022-11-14 20:12:16 -0300

    Agregando parrafo

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Hola Mundo</h1>
+        <p>Primer deploy Vercel y next</p>
       </main>
     </div>
   );
